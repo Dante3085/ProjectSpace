de96f9aea6503d3dc3c29e99c99782e2357b5126
theSymbol255 <manuelrottmann@googlemail.com>
2019-09-04 14:19:42 +0200

    textbox absatz warte lese

--- src/gui/TextBox.cpp
+++ src/gui/TextBox.cpp
@@ -6,7 +6,7 @@
 namespace ProjectSpace
 {
 	TextBox::TextBox(std::string texturePath, sf::String str, sf::Vector2f size, sf::Vector2f position)
-		: str{ str }, writtenStr{ "" }, position{ position }, padding{ 0 }, elapsedMillis{ 0 }, currentPos{ 0 }, umbruchZaehler{0}
+		: str{ str }, writtenStr{ "" }, position{ position }, padding{ 0 }, elapsedMillis{ 0 }, currentPos{ 0 }, umbruchZaehler{ 0 }, waitFlag{ false }
 	{
 		texture.loadFromFile(texturePath);
 		rec.setTexture(&texture);
@@ -18,7 +18,7 @@ namespace ProjectSpace
 		font.loadFromFile("rsrc/fonts/joystix_monospace.ttf");
 		text.setFont(font);
 		text.setFillColor(sf::Color(0, 255, 190, 255));
-		parseString(this->str, (size.x / 25)+1, size.y/36.5);//zeilenanzahl =~ size / 36,5
+		parseString(this->str, (size.x / 25) + 1, size.y / 36.5);//zeilenanzahl =~ size / 36,5
 		text.setString(writtenStr);
 		rec.setPosition(position);
 		text.setPosition(position);
@@ -34,16 +34,27 @@ namespace ProjectSpace
 	{
 
 		elapsedMillis += time.asMilliseconds();
-		if (elapsedMillis > 20 && currentPos < str.getSize()) {
-			elapsedMillis = 0;
-			if (str[currentPos] == '\n') umbruchZaehler++;
-			if (umbruchZaehler >= (rec.getSize().y / 36.5)-1) { 
-				umbruchZaehler = 0;
-				writtenStr = "";
-				currentPos++;
+		if (waitFlag) {
+			if (elapsedMillis >= 2000) {
+				elapsedMillis = 0;
+				waitFlag = false;
 			}
-			writtenStr += str[currentPos++];
-			text.setString(writtenStr);
+		}
+		else {
+			if (elapsedMillis > 20 && currentPos < str.getSize()) {
+				elapsedMillis = 0;
+				if (str[currentPos] == '\n') umbruchZaehler++;
+				if (umbruchZaehler >= (rec.getSize().y / 36.5) - 1) {
+					umbruchZaehler = 0;
+					writtenStr = "";
+					currentPos++;
+					waitFlag = true;
+				}
+				else {
+					writtenStr += str[currentPos++];
+					text.setString(writtenStr);
+				}
+			}
 		}
 		//	// TODO(moritz):
 	}
