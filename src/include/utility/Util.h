2f09994419aa851c24cd84de2ea1ed5361dba08f
mjsch <moritz97.s@web.de>
2019-09-05 20:11:22 +0200

    util template functions for sf::Vector2<T>, sf::Rect<T> and std::vector<T>

--- src/include/utility/Util.h
+++ src/include/utility/Util.h
@@ -7,32 +7,79 @@
 #include <SFML/System/String.hpp>
 #include <SFML/Graphics/VertexArray.hpp>
 #include <vector>
+#include <string>
+#include <iostream>
 
 #include "collision/Line2F.h"
 #include "scenes/Scene.h"
 
 namespace ProjectSpace
 {
-	std::ostream& operator<<(std::ostream& stream, sf::Vector2f const& v);
-	std::ostream& operator<<(std::ostream& stream, sf::FloatRect const& r);
-	float vectorLength(sf::Vector2f const& v);
-	sf::Vector2f vectorMultScalar(sf::Vector2f const& v, float scalar);
-
-	// Adds scalar to all of v's coordinates.
-	sf::Vector2f operator+(sf::Vector2f const& v, float scalar);
-	sf::Vector2f operator*(sf::Vector2f const& v, float scalar);
-	sf::Vector2f operator-(sf::Vector2f const& v1, sf::Vector2f const& v2);
-	sf::Vector2f operator/(sf::Vector2f const& v1, sf::Vector2f const& v2);
-	float scalarProduct(sf::Vector2f const& v1, sf::Vector2f const& v2);
-	sf::Vector2f normalizedVector(sf::Vector2f const& v);
+	// sf::Vector2<T>
+	template <class T>
+	std::ostream& operator<<(std::ostream& stream, sf::Vector2<T> const& v)
+	{
+		return stream << "{ " << v.x << ", " << v.y << " }";
+	}
 
 	template <class T>
-	std::ostream& operator<<(std::ostream& stream, sf::Vector2<T> v);
+	std::string toString(sf::Vector2<T> const& v)
+	{
+		return "{ " += std::to_string(v.x) += ", " += std::to_string(v.y) += " }";
+	}
 
-	std::string toString(sf::Vector2f const& v);
-	
 	template <class T>
-	std::ostream& operator<<(std::ostream& stream, std::vector<T> v);
+	sf::Vector2<T> operator/(sf::Vector2<T> const& v1, sf::Vector2<T> const& v2)
+	{
+		return sf::Vector2<T>{ v1.x / v2.x, v1.y / v2.y };
+	}
+
+	template <class T, class U>
+	sf::Vector2<T> operator+(sf::Vector2<T> const& v, U scalar)
+	{
+		return sf::Vector2<T>{ v.x + scalar, v.y + scalar };
+	}
+
+	template <class T>
+	sf::Vector2<T> operator-(sf::Vector2<T> const& v, float scalar)
+	{
+		return sf::Vector2<T>{ v.x - scalar, v.y - scalar };
+	}
+
+	template <class T>
+	T vectorLength(sf::Vector2<T> const& v)
+	{
+		return sqrt(v.x * v.x + v.y * v.y);
+	}
+
+	template <class T>
+	float scalarProduct(sf::Vector2<T> const& v1, sf::Vector2<T> const& v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	template <class T>
+	sf::Vector2<T> normalizedVector(sf::Vector2<T> const& v)
+	{
+		return v / vectorLength(v);
+	}
+
+	template <class T>
+	std::ostream& operator<<(std::ostream& stream, sf::Rect<T> const& r)
+	{
+		return stream << "{ " << r.left << ", " << r.top << ", " << r.width << ", " << r.height << " }";
+	}
+
+	template <class T>
+	std::ostream& operator<<(std::ostream& stream, std::vector<T> v)
+	{
+		stream << "{ ";
+		for (T const& t : v)
+		{
+			stream << t << " ";
+		}
+		stream << "}";
+	}
 
 	// Inserts \n into text according to given lineLength, so that text wraps in between words.
 	void addLineWrapping(sf::String& text, int lineLength);
--- src/utility/Util.cpp
+++ src/utility/Util.cpp
@@ -1,95 +1,10 @@
 
 #include "utility/Util.h"
 
-#include <iostream>
 #include <math.h>
-#include <vector>
-#include <string>
 
 namespace ProjectSpace
 {
-	std::ostream& operator<<(std::ostream& stream, sf::Vector2f const& v)
-	{
-		stream << "{" << v.x << ", " << v.y << "}";
-		return stream;
-	}
-
-	std::ostream& operator<<(std::ostream& stream, sf::FloatRect const& r)
-	{
-		stream << "{{" << r.left << ", " << r.top << "}, {" << r.left + r.width << ", " << r.top <<"}, {"
-		<< r.left + r.width << ", " << r.top + r.height << "}, {" << r.left << ", " << r.top + r.height << "}}";
-		return stream;
-	}
-
-	float vectorLength(sf::Vector2f const& v)
-	{
-		return sqrt(pow(v.x, 2) + pow(v.y, 2));
-	}
-
-	sf::Vector2f vectorMultScalar(sf::Vector2f const& v, float scalar)
-	{
-		return sf::Vector2f{ v.x * scalar, v.y * scalar };
-	}
-
-	sf::Vector2f operator+(sf::Vector2f const& v, float scalar)
-	{
-		return sf::Vector2f{ v.x + scalar, v.y + scalar };
-	}
-
-	sf::Vector2f operator*(sf::Vector2f const& v, float scalar)
-	{
-		return sf::Vector2f{ v.x * scalar, v.y * scalar };
-	}
-
-	sf::Vector2f operator-(sf::Vector2f const& v1, sf::Vector2f const& v2)
-	{
-		return sf::Vector2f{ v1.x - v2.x, v1.y - v2.y };
-	}
-
-	sf::Vector2f operator/(sf::Vector2f const& v1, sf::Vector2f const& v2)
-	{
-		return sf::Vector2f{ v1.x / v2.x, v1.y / v2.y };
-	}
-
-	float scalarProduct(sf::Vector2f const& v1, sf::Vector2f const& v2)
-	{
-		return (v1.x * v2.x) + (v1.y * v2.y);
-	}
-
-	sf::Vector2f normalizedVector(sf::Vector2f const& v)
-	{
-		return vectorMultScalar(v, 1.f / vectorLength(v));
-	}
-
-	template <class T>
-	std::ostream& operator<<(std::ostream& stream, sf::Vector2<T> v)
-	{
-		stream << "{" << v.x << ", " << v.y << "}";
-		return stream;
-	}
-
-	std::string toString(sf::Vector2f const& v)
-	{
-		std::string str{"{ "};
-		str.append(std::to_string(v.x));
-		str.append(", ");
-		str.append(std::to_string(v.y));
-		str.append("}");
-		return str;
-	}
-
-	template <class T>
-	std::ostream& operator<<(std::ostream& stream, std::vector<T> v)
-	{
-		stream << "{ ";
-		for (T& t : v)
-		{
-			stream << t << " ";
-		}
-		stream << "}";
-		return stream;
-	}
-
 	void addLineWrapping(sf::String& text, int lineLength)
 	{
 		int writer = 0;
