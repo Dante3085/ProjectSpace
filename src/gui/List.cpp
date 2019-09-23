8ed4437aa165dd8e989075a4e0a7f7c7fab66aa4
mjsch <moritz97.s@web.de>
2019-09-23 14:18:10 +0200

    gui/List TODO update.

--- src/gui/List.cpp
+++ src/gui/List.cpp
@@ -10,8 +10,9 @@ namespace ProjectSpace
 {
 	int List::numLists = 0;
 
-	// TODO: Mit mittlerem MouseButton durch die Liste scrollen.
-	// TODO: Localizer soll auch auf Selektion mit Maus reagieren.
+	// TODO: Disable Mouse selection when scrolling with Mouse Wheel.
+	// TODO: Don't scroll beyond beginning or end of List. This not being the case makes it annoying
+	// when scrolling through large parts of the List and then accidentally going over the end or beginning.
 
 	// I am not doing bounds{position.x, position.y, -1, -1} because topText will be set to position
 	// and sf::Text::getPosition() will not return the upperLeft corner of visible sf::Text, only
